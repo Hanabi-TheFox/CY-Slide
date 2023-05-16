6d46c345c5c648c6e790e32b23ba7dd0e1f7e10f
Hanabi-TheFox <pintoethan@cy-tech.fr>
2023-05-16 18:05:25 +0200

    Starting Page Made

--- CY_Slide/src/main/java/com/cyslide/HelloApplication.java
+++ CY_Slide/src/main/java/com/cyslide/HelloApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 450);
+        stage.setResizable(false);
         stage.setTitle("CY-Slide Game");
         stage.setScene(scene);
         stage.show();
