57d881220daa18f6fbbc854b7a2c695867e6b86d
Hanabi-TheFox <pintoethan@cy-tech.fr>
2023-05-16 18:50:04 +0200

    Change Application Name

--- CY_Slide/src/main/java/com/cyslide/CySlideApplication.java
+++ CY_Slide/src/main/java/com/cyslide/CySlideApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CySlideApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CySlideApplication.class.getResource("StartPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 450);
         stage.setResizable(false);
         stage.setTitle("CY-Slide Game");
