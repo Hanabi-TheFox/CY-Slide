a20b76778f7083e5d0c103e1a20ce008808de919
Ymasuu <samy.belbouab95@gmail.com>
2023-05-17 18:22:03 +0200

    adjustment main.java

--- CY_Slide/src/main/java/com/cyslide/Model/Main.java
+++ CY_Slide/src/main/java/com/cyslide/Model/Main.java
@@ -2,9 +2,10 @@ package com.cyslide.Model;
 // ce main permet de tester nos fonction java
 public class Main{
     public static void main(String[] args){
-        Level level = new Level(3);
+        Level level = new Level(9);
         for (int i=0; i < level.getTable().length; i++){
             for (int j=0; j < level.getTable().length; j++){
+                System.out.println("case [" + i + "][" + j +"]");
                 level.getTable()[i][j].showTile();
             }
         }
