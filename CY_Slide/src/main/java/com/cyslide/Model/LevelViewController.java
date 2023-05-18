8736c3f4d499c45f844d8550222881e660fe28da
RDNATOS <78343596+RDNATOS@users.noreply.github.com>
2023-05-18 16:24:17 +0200

    some classes

---
+++ CY_Slide/src/main/java/com/cyslide/Model/LevelViewController.java
@@ -0,0 +1,13 @@
+package com.cyslide.Model;
+
+import javafx.fxml.FXML;
+
+public class LevelViewController {
+    
+    
+    
+    @FXML
+    protected void initialize() {
+        //TODO
+    }
+}
