bf1b0c2a43d4b4375266606c2ebd3a9d362af630
Ymasuu <samy.belbouab95@gmail.com>
2023-05-19 18:09:14 +0200

    planning.txt

--- CY_Slide/src/main/java/com/cyslide/Model/EmptyTile.java
+++ CY_Slide/src/main/java/com/cyslide/Model/EmptyTile.java
@@ -17,28 +17,28 @@ public class EmptyTile extends Tile {
     }
 
     @Override
-    public void move(String direction, RectangleWithLabel [][] table){
+    public void move(String direction, RectangleWithLabel [][] table) {
         if(direction=="UP"){
-            // we go to the top of the matrix so we must reduce the position of the first tab which is table[]
-           // We change our table
-           SwapRectangles(getPosX(), getPosY(), getPosX()-1, getPosY(), table);
-           // Now we change coordinates of the number tile
-           table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-           // Now we set coordinate of the empty tile
-           setPosX(getPosX()-1);
-       }else if(direction=="DOWN"){
+                // we go to the top of the matrix so we must reduce the position of the first tab which is table[]
+            // We change our table
+            SwapRectangles(getPosX(), getPosY(), getPosX()-1, getPosY(), table);
+            // Now we change coordinates of the empty tile
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            // Now we set coordinate of the number tile
+            table[getPosX()-1][getPosY()].GetTile().moved(getPosX()-1, getPosY());
+        }else if(direction=="DOWN"){
             SwapRectangles(getPosX(), getPosY(), getPosX()+1, getPosY(), table);
-           table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-           setPosX(getPosX()+1);
-       }else if(direction=="LEFT"){ // we go to the left of the matrix so we must reduce the position of the second tab which is table[][]
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            table[getPosX()+1][getPosY()].GetTile().moved(getPosX()+1, getPosY());
+        }else if(direction=="LEFT"){ // we go to the left of the matrix so we must reduce the position of the second tab which is table[][]
             SwapRectangles(getPosX(), getPosY(), getPosX(), getPosY()-1, table);
-           table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-           setPosY(getPosY()-1);
-       }else if(direction=="RIGHT"){
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            table[getPosX()][getPosY()-1].GetTile().moved(getPosX(), getPosY()-1);
+        }else if(direction=="RIGHT"){
             SwapRectangles(getPosX(), getPosY(), getPosX(), getPosY()+1, table);
-           table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-           setPosY(getPosY()+1);
-       }
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            table[getPosX()][getPosY()+1].GetTile().moved(getPosX(), getPosY()+1);
+        }
     }
     public void SwapRectangles(int i1, int j1,int i2, int j2, RectangleWithLabel[][] tab){
         RectangleWithLabel tmp = tab[i1][j1];
--- CY_Slide/src/main/java/com/cyslide/Model/NumberTile.java
+++ CY_Slide/src/main/java/com/cyslide/Model/NumberTile.java
@@ -11,52 +11,52 @@ public class NumberTile extends Tile{
         this.number = number;
     }
 
-        @Override
-        public void move(String direction, RectangleWithLabel [][] table) {
-            if(direction=="UP"){
-                 // we go to the top of the matrix so we must reduce the position of the first tab which is table[]
-                // We change our table
-                SwapRectangles(getPosX(), getPosY(), getPosX()-1, getPosY(), table);
-                // Now we change coordinates of the empty tile
-                table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-                // Now we set coordinate of the number tile
-                table[getPosX()-1][getPosY()].GetTile().moved(getPosX()-1, getPosY());
-            }else if(direction=="DOWN"){
-                SwapRectangles(getPosX(), getPosY(), getPosX()+1, getPosY(), table);
-                table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-                table[getPosX()+1][getPosY()].GetTile().moved(getPosX()+1, getPosY());
-            }else if(direction=="LEFT"){ // we go to the left of the matrix so we must reduce the position of the second tab which is table[][]
-                SwapRectangles(getPosX(), getPosY(), getPosX(), getPosY()-1, table);
-                table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-                table[getPosX()][getPosY()-1].GetTile().moved(getPosX(), getPosY()-1);
-            }else if(direction=="RIGHT"){
-                SwapRectangles(getPosX(), getPosY(), getPosX(), getPosY()+1, table);
-                table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
-                table[getPosX()][getPosY()+1].GetTile().moved(getPosX(), getPosY()+1);
-            }
+    @Override
+    public void move(String direction, RectangleWithLabel [][] table) {
+        if(direction=="UP"){
+                // we go to the top of the matrix so we must reduce the position of the first tab which is table[]
+            // We change our table
+            SwapRectangles(getPosX(), getPosY(), getPosX()-1, getPosY(), table);
+            // Now we change coordinates of the empty tile
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            // Now we set coordinate of the number tile
+            table[getPosX()-1][getPosY()].GetTile().moved(getPosX()-1, getPosY());
+        }else if(direction=="DOWN"){
+            SwapRectangles(getPosX(), getPosY(), getPosX()+1, getPosY(), table);
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            table[getPosX()+1][getPosY()].GetTile().moved(getPosX()+1, getPosY());
+        }else if(direction=="LEFT"){ // we go to the left of the matrix so we must reduce the position of the second tab which is table[][]
+            SwapRectangles(getPosX(), getPosY(), getPosX(), getPosY()-1, table);
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            table[getPosX()][getPosY()-1].GetTile().moved(getPosX(), getPosY()-1);
+        }else if(direction=="RIGHT"){
+            SwapRectangles(getPosX(), getPosY(), getPosX(), getPosY()+1, table);
+            table[getPosX()][getPosY()].GetTile().moved(getPosX(), getPosY());
+            table[getPosX()][getPosY()+1].GetTile().moved(getPosX(), getPosY()+1);
         }
+    }
 
-        @Override
-        public boolean mouvementAvailable(int x, int y, String direction, RectangleWithLabel [][] table) {
-            if (direction.equals("UP")) {
-                if (getPosX() == 0 || table[x-1][y].GetTile().getType() != -1) {
-                    return false;
-                }
-            } else if (direction.equals("DOWN")) {
-                if (getPosX() == table.length - 1 || table[x+1][y].GetTile().getType() != -1) {
-                    return false;
-                }
-            } else if (direction.equals("RIGHT")) {
-                if (getPosY() == table[getPosX()].length - 1 || table[x][y+1].GetTile().getType() != -1) {
-                    return false;
-                }
-            } else if (direction.equals("LEFT")) {
-                if (getPosY() == 0 || table[x][y-1].GetTile().getType() != -1) {
-                    return false;
-                }
+    @Override
+    public boolean mouvementAvailable(int x, int y, String direction, RectangleWithLabel [][] table) {
+        if (direction.equals("UP")) {
+            if (getPosX() == 0 || table[x-1][y].GetTile().getType() != -1) {
+                return false;
             }
-            return true;
+        } else if (direction.equals("DOWN")) {
+            if (getPosX() == table.length - 1 || table[x+1][y].GetTile().getType() != -1) {
+                return false;
+            }
+        } else if (direction.equals("RIGHT")) {
+            if (getPosY() == table[getPosX()].length - 1 || table[x][y+1].GetTile().getType() != -1) {
+                return false;
+            }
+        } else if (direction.equals("LEFT")) {
+            if (getPosY() == 0 || table[x][y-1].GetTile().getType() != -1) {
+                return false;
+            }
         }
+        return true;
+    }
     public void SwapRectangles(int i1, int j1,int i2, int j2, RectangleWithLabel[][] tab){
         RectangleWithLabel tmp = tab[i1][j1];
         tab[i1][j1] = tab[i2][j2];
